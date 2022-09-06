b8eeb855e7af5fc2c129b22c360f1cd216eec10b
Codymani <codybank91@gmail.com>
2022-09-06 10:54:41 +0530

    mastercopy

---
+++ src/test/java/org/branches/Ambrose.java
@@ -0,0 +1,8 @@
+package org.branches;
+
+public class Ambrose {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+}
---
+++ src/test/java/org/branches/Mani.java
@@ -0,0 +1,7 @@
+package org.branches;
+
+public class Mani {
+public static void main(String[] args) {
+	System.out.println("hello");
+}
+}
