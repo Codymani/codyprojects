97cffcaa28715d5d2e5af41fe582866694d46131
ambijoe26 <ambijoe26@gmail.com>
2022-09-06 22:05:33 +0530

    Ambrose Copy

--- src/test/java/org/branches/Ambrose.java
+++ src/test/java/org/branches/Ambrose.java
@@ -4,5 +4,7 @@ public class Ambrose {
 
 	public static void main(String[] args) {
 		System.out.println("hi");
+		System.out.println("Hello This is Ambrose");
+		System.out.println("How Are You?");
 	}
 }
