29c3a154c56931b2a15e5461699e6b0ad44a7c5b
Monisha730520 <monitom234@gmail.com>
2022-09-08 10:15:51 +0530

    project structure monitom

---
+++ src/test/java/org/branches/Monitom.java
@@ -0,0 +1,10 @@
+package org.branches;
+
+public class Monitom {
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("hello hi");
+		System.out.println("hello im monisha");
+	}
+
+}
